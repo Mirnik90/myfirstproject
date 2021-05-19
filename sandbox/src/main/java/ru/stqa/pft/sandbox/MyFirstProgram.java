c9f1aa82b002f42de42be66f585bdab88e0364bd
Mirnik90 <kostya.mir57@gmail.com>
2021-05-19 14:14:06 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,13 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    hello("world"); // Создали функцию которая будет вызывать сообщение "Hello World"
+    hello("user");
+    hello("Mirnik");
+
+    double l = 5;
+    System.out.println("Площадь квадрата ос стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+  public static void hello (String somebody) { // Создали функцию которая будет вызывать сообщение "Hello World"
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
 
-//# Файл с исходным кодом
-//# Название класса должно совпадать с названием файла
-//# Начало и конец функции обозначаются фигурными скобками
-//# Программа состоит из класса внутри которого есть функция MAIN и она содержит исполняемый код 
-//# Компиляция программы - трансляция исходного кода в байт код, который будет запускаться с помощью интерпретатора байт кода 
-//# Названия классов начинаются с большой буквы
